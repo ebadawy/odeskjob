5bb8c7c944464b8b55692427203bc97dfc7b6805
ehab93 <ehalbadawy93@gmail.com>
2014-12-27 13:00:19 +0200

    Edit Carrierwave config file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,20 @@
 CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:               'AWS',
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                 ENV['FOG_REGION']
+  }
 
-    config.fog_credentials = {
-      :provider                 => 'AWS',
-      :aws_access_key_id        => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                   => ENV['FOG_REGION']
-    }
-    config.fog_directory   = ENV['FOG_DIRECTORY']
-    config.fog_public      = true
-    config.fog_attributes  = {'Cache-Control' => 'max-age=315576000'}
-    # if Rails.env.production?
-    #   config.asset_host      = ""
-    # end
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage           = :file
+    config.enable_processing = false
+    config.root              = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
 
+  config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
+  config.fog_directory    = ENV['FOG_DIRECTORY']
 end
